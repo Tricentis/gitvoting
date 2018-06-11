9fce80bbb5949c1a3ec91520f36267f91ad05c96
Michael Niessl <mniessl3@gatech.edu>
2018-06-11 21:07:06 +0200

    archive, projects, layout, favicon

---
+++ GIG/Controllers/ArchiveController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp_WSFederation_DotNet.Controllers
+{
+    
+    public class ArchiveController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
