e718ab225e18d45cf1b1d81c46dbff638b603f3d
Michael Niessl <mniessl3@gatech.edu>
2018-06-14 10:48:43 +0200

    archive

--- GIG/Controllers/ArchiveController.cs
+++ GIG/Controllers/ArchiveController.cs
@@ -1,17 +1,66 @@
-﻿using System;
+﻿using GIG.Data;
+using GIG.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 
 namespace GIG.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class ArchiveController : Controller
     {
-        public ActionResult Index()
+        private GIGContext db = new GIGContext();
+        private List<string> archivedgigs = WebConfigurationManager.AppSettings["ArchivedGIGs"].Split(',').ToList();
+
+        public ActionResult Index(string year)
         {
-            return View();
+            ViewData["archives"] = archivedgigs;
+
+            if (string.IsNullOrEmpty(year)) {
+                if (archivedgigs.Count > 0) {
+                    year = archivedgigs.Last();
+                }
+            }
+
+            if (!archivedgigs.Contains(year)) {
+                return View();
+            }
+
+            ViewData["gig"] = year;
+            
+            var votes = db.Votes
+                   .Where(v => v.Year == year)
+                   .GroupBy(v => v.Team)
+                    .Select(g => new { video = g.Key, count = g.Count() }).ToDictionary(t => t.video, t => t.count);
+
+            var votedByMe = db.Votes
+                    .Where(v => v.Year == year && v.Username == User.Identity.Name)
+                    .Select(v => new { v.Team })
+                    .ToDictionary(d => d.Team, d => d.Team);
+
+            List<Video> videos = new List<Video>();
+            try {
+                using (StreamReader sr = new StreamReader(Server.MapPath($"~/Content/{year}/info.json"))) {
+                    videos = JsonConvert.DeserializeObject<List<Video>>(sr.ReadToEnd());
+                }
+            } catch {
+                // no videos
+            }
+
+            foreach (Video video in videos) {
+                if (votes.ContainsKey(video.Team)) {
+                    video.Votes = votes[video.Team];
+                }
+
+                video.VotedByMe = votedByMe.ContainsKey(video.Team);
+            }
+
+            return View(videos);
         }
     }
 }
