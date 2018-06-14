cdbd9376c82ccd0ab0a79396206b90c2b314408b
Michael Niessl <mniessl3@gatech.edu>
2018-06-14 10:49:27 +0200

    authorization in archive

--- GIG/Controllers/ArchiveController.cs
+++ GIG/Controllers/ArchiveController.cs
@@ -11,7 +11,7 @@ using System.Web.Mvc;
 
 namespace GIG.Controllers
 {
-    //[Authorize]
+    [Authorize]
     public class ArchiveController : Controller
     {
         private GIGContext db = new GIGContext();
